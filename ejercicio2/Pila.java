22a3cd1098d5349ef79c145c577b981b5e5bc9e5
Edson López <elopezqu@unsa.edu.pe>
2022-06-05 12:47:25 -0500

    Se agrega los metodos peek, pop, push

--- ejercicio2/Pila.java
+++ ejercicio2/Pila.java
@@ -7,4 +7,17 @@ public class Pila<T>{
     public boolean empty(){
         return this.top == null;
     } 
+    public T peek(){
+        return this.top.obtenerValor();
+    }
+    public T pop(){
+        T ultimo = this.top.obtenerValor();
+        top = this.top.obtenerSiguiente();
+        return ultimo;
+    }
+    public void push(T dato){
+        Node<T> nuevo = new Node<T>(dato);
+        nuevo.enlazarSiguiente(top);
+        top = nuevo;
+    }
 }
